09283a1d58953e6cd38715a44b08554edb86fec0
Ashvin Narayanan <ashvin@smilingmind.com.au>
2020-05-16 13:14:37 +0930

    Added a vanilla Xamarin.Mac app and updated the README with repro instructions.

---
+++ AppNapRepro/AppDelegate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using AppKit;
+using Foundation;
+
+namespace AppNapRepro
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : NSApplicationDelegate
+    {
+        private NSWindow _window;
+        private NSObject _activity;
+        private DateTime _prevTime;
+
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable |
+                        NSWindowStyle.Miniaturizable |
+                        NSWindowStyle.Resizable |
+                        NSWindowStyle.Titled;
+            var rect = new CoreGraphics.CGRect(100, 100, 1024, 768);
+            _window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            _window.Title = "AppNapRepro";
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            _activity = NSProcessInfo.ProcessInfo.BeginActivity(
+                            NSActivityOptions.Background |
+                            NSActivityOptions.LatencyCritical,
+                            "You charge $3500AUD for this laptop and yet " +
+                            "can't provide enough resources for a smooth " +
+                            "operation of this app in the background? " +
+                            "Eat a dick Apple."
+                        );
+
+            Task.Factory.StartNew(async () =>
+            {
+                while (true)
+                {
+                    var now = DateTime.Now;
+                    var now_str = now.ToString("HH:mm:ss.fff");
+                    Debug.WriteLine(now_str);
+                    var idle_time = now - _prevTime;
+                    if (idle_time.TotalMilliseconds > 10000)
+                    {
+                        Debug.WriteLine("FFSakes Apple!");
+                    }
+                    _prevTime = now;
+                    await Task.Delay(1000);
+                }
+            });
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            NSProcessInfo.ProcessInfo.EndActivity(_activity);
+            _activity = null;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # AppNapRepro
-Repro of a vanilla Xamarin.Mac app napping despite instructing it not to
+Repro of a vanilla Xamarin.Mac app napping despite being instructed not to.
+
+## Steps to reproduce
+
+1. On a Mac, open the .sln file using Visual Studio for Mac 2019.
+2. Hit Play, i.e., run the app in Debug mode.
+3. Minimise the app and cover Visual Studio (VS) with another application (not sure if it's significant but that's what I did).
+4. Lock the Mac, go away, do stuff for about half an hour.
+5. Return to your Mac, unlock, and check the Application Output panel in VS.
+
+## Expected result
+
+6. There should not be a debug line reading: FFSakes Apple!
+
+## Actual result
+
+6. There is.
+
+## Environment
+
+OS: macOS Catalina, 10.15.4
+Processor: 2.4 GHz Quad-Core Intel Core i5
